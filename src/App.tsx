8eceb0f5fc3ba241750ef642b8486292315013b8
MilanKnezevic <milan011knezevic@gmail.com>
2021-08-02 20:41:00 +0200

    first task finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Company } from "./components/Company";
 import { ICompany } from "./types/companies";
 
 function App() {
@@ -20,23 +21,41 @@ function App() {
   const handleOnNumberChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const number = event.target.value;
     setRatting(+number);
-    
   };
 
   const moveCompanyWithRatting = () => {
-    
+    if (companies) {
+
+      const sortedCompanies = [...companies];
+
+      let left = 0;
+      let right = sortedCompanies.length - 1;
+
+      while (left < right) {
+        while (left < right && ratting === sortedCompanies[right].ratings) right--;
+        if (ratting === sortedCompanies[left].ratings)swap(left, right, sortedCompanies);
+          left++;        
+      }
+      setCompanies(sortedCompanies);
+    }
+  };
+
+  const swap = (left: number, right: number, companies: ICompany[]) => {
+    let temp = companies[left];
+    companies[left] = companies[right];
+    companies[right] = temp;
   };
 
   return (
     <div className="App">
       <div className="row">
-        <h3>Move all companies with selected rating to end of list</h3>
+        <h1>Move all companies with selected rating to end of list</h1>
         <label className="fw-bold" htmlFor="company-ratting">
           Numbers between 1 and 10 include 10
         </label>
         <input
           id="company-ratting"
-          className="p-3 col-3"
+          className="p-2 col-2"
           type="number"
           min="1"
           max="10"
@@ -46,11 +65,22 @@ function App() {
         />
       </div>
 
-
-
-      <button className="d-block btn btn-primary mt-5" onClick={moveCompanyWithRatting}>
+      <button
+        className="d-block btn btn-primary mt-4"
+        onClick={moveCompanyWithRatting}
+      >
         Move companies with ratting
       </button>
+      
+      {companies?.map((company) => (
+        <Company
+          key={company.id}
+          id={company.id}
+          company={company.company}
+          address={company.address}
+          ratings={company.ratings}
+        />
+      ))}
     </div>
   );
 }
