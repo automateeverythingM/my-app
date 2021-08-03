d38a7cf8e62f17ad34225d2cc2b37590da6730b7
automateeverythingM <markospavicbg@gmail.com>
2021-08-03 12:28:36 +0200

    readme

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { NumberBox } from "./components/NumberBox";
 import { generateArray, generateRandomNumber } from "./helpers/generator";
 
-
 function App() {
-  
-  const [firstArray, setFirstArray] = useState<number[]>(generateArray(15, -15,  10));
-  const [secondArray, setSecondArray] = useState<number[]>(generateArray(15, -15,  10));
+  const [firstArray, setFirstArray] = useState<number[]>(
+    generateArray(15, -15, 12)
+  );
+  const [secondArray, setSecondArray] = useState<number[]>(
+    generateArray(15, -15, 12)
+  );
   const [target, setTarget] = useState<number>();
 
   function generateTarget() {
@@ -14,27 +16,34 @@ function App() {
   }
 
   function generateNewArrays() {
-    setFirstArray(generateArray(15, -15, 10));
-    setSecondArray(generateArray(15, -15, 10));
+    setFirstArray(generateArray(15, -15, 12));
+    setSecondArray(generateArray(15, -15, 12));
   }
-
-
 
   return (
     <div className="App row container mx-auto">
       <div className="col-3 d-flex flex-column justify-content-center align-items-center">
-        {firstArray?.map((number)=><NumberBox num={number}/>)}
+        {firstArray?.map((number) => (
+          <NumberBox num={number} />
+        ))}
       </div>
-      <div className="col-6 d-flex flex-column vh-100 align-items-center justify-content-center fw-bold" >
-
-          <div className="d-block"  style={{fontSize:"8rem"}}>
-            {target}
-          </div>
-          <button onClick={generateTarget} className="btn btn-warning d-block">Generate Target</button>
-          <button onClick={generateNewArrays} className="btn btn-warning d-block">Generate New Arrays</button>
+      <div className="col-6 d-flex flex-column vh-100 align-items-center justify-content-center fw-bold">
+        <div></div>
+        <div className="row d-block" style={{ fontSize: "8rem" }}>
+          <div className="col">{target}</div>
+          <div className="col"></div>
+        </div>
+        <button onClick={generateTarget} className="btn btn-warning d-block">
+          Generate Target
+        </button>
+        <button onClick={generateNewArrays} className="btn btn-warning d-block">
+          Generate New Arrays
+        </button>
       </div>
       <div className="col-3  d-flex flex-column justify-content-center align-items-center">
-      {secondArray?.map((number)=><NumberBox num={number}/>)}
+        {secondArray?.map((number) => (
+          <NumberBox num={number} />
+        ))}
       </div>
     </div>
   );
